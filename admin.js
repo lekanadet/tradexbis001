375f3a181fe1a5f122ad79a618e3c279083e5005
leaknadet <adetutu.olalekan@yahoo.com>
2022-06-22 15:17:42 +0100

    currency updatess

--- admin.js
+++ admin.js
@@ -3,19 +3,11 @@ const path = require('path')
 const express = require('express')
 var router = express.Router();
 var db = require('./database/db.js');
-<<<<<<< HEAD
-=======
-const findout = require('./middleware/checks.js')
-const { check,validationResult } = require('express-validator');
-const nodemailer = require('nodemailer');
-const jwt = require("jsonwebtoken");
->>>>>>> d8451a8219e34b8ead58cf572d812b493b10ea3f
 const validateLoginMiddlewareCookie = require('./middleware/validate_login_cookie.js');
 
 
 
 
-<<<<<<< HEAD
 /* protected home route for currency caategories or type */
 router.get('/currency-type',validateLoginMiddlewareCookie.isLoggedIn,(req,res) => { // an example of a protected route
   if (req.userData) { 
@@ -131,21 +123,5 @@ value = [currency_id,currency_name,currency_code, currency_symbol,currency_type,
     } 
   
    })      
-=======
-
-  /* protected home route for backend testing */
-  router.get('/currency-type',validateLoginMiddlewareCookie.isLoggedIn,(req,res) => { // an example of a protected route
-    if (req.userData) { 
-      console.log(req.userData)
-
-      db.query("CALL get_currency_types();", function (err, result){
-        if (err) throw err;
-
-      res.json({message: "Currency Types",Currency_Type: result[0][0].currency_type,})
-      })
-      } 
-    
-     }) 
->>>>>>> d8451a8219e34b8ead58cf572d812b493b10ea3f
 
    module.exports = router;   
--- middleware/validate_login_cookie.js
+++ middleware/validate_login_cookie.js
@@ -2,11 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
 
-<<<<<<< HEAD
 isLoggedIn: (req, res, next) => {
-=======
-isLoggedIn: async (req, res, next) => {
->>>>>>> d8451a8219e34b8ead58cf572d812b493b10ea3f
   const token = req.cookies.access_token3;
   if (!token) {
     return res.status(401).send({
@@ -14,11 +10,7 @@ isLoggedIn: async (req, res, next) => {
     });
   }
   try {
-<<<<<<< HEAD
     const data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-=======
-    const data = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
->>>>>>> d8451a8219e34b8ead58cf572d812b493b10ea3f
     req.userData = data
     return next();
   } catch (err) {
